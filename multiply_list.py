1dd09ce0d118797077b91cf0e49ec4e456dd5b10
ilyas <ilyasutemisov@gmail.com>
2023-08-21 23:04:06 +0200

    done

--- multiply_list.py
+++ multiply_list.py
@@ -1,8 +1,9 @@
-dig_list = [2, 0 , 4, 7, 8]
-result =1
-for i in range(0,len(dig_list),2):
-    result *= dig_list[i]
-result *= dig_list[-1]
+dig_list = []
+result = 1
 if len(dig_list) == 0:
     result = 0
+else:
+    for i in range(0,len(dig_list),2):
+        result *= dig_list[i]
+    result *= dig_list[-1]
 print(result)
