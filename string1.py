8d772ee5bfcc76b1d9e4c097c7223083e2b5a029
ilyas <ilyasutemisov@gmail.com>
2023-08-28 18:53:25 +0200

    done

--- string1.py
+++ string1.py
@@ -1,16 +1,13 @@
 import keyword
-checking_str = input("")
-def checking_char(s):
-    if s[0].isdigit():
-        return False
-    elif not s.isidentifier():
-        return False
-    elif s in keyword.kwlist:
-        return False
-    else:
-        return True
+s = input("")
+if s[0].isdigit():
+    print(False)
+elif not s.isidentifier():
+    print(False)
+elif s in keyword.kwlist:
+    print(False)
+else:
+    print(True)
 
-if checking_char(checking_str):
-        print('True')
-else: print('You wrote wrong string')
+
 
